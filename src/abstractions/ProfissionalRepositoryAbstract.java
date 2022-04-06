aa9c342042182be22952737d73424ba6128021fc
RodVarela <rodvarela.dev@gmail.com>
2022-04-05 21:59:06 -0300

    first commit

---
+++ src/abstractions/ProfissionalRepositoryAbstract.java
@@ -0,0 +1,16 @@
+package abstractions;
+
+import entities.Profissional;
+
+public abstract class ProfissionalRepositoryAbstract {
+	
+	//atributo para armazenar o caminho do
+	//diretório para onde os aquivos
+	//serão exportados
+	protected static final String PATH = "c:\\temp\\"; //CONSTANTE!
+	
+	//Método abstrato que deverá ser implementado
+	//para realizar a exportação dos arquivos
+	public abstract void exportar(Profissional profissional) throws Exception;
+	
+}
